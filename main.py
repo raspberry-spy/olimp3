db2fb9b8ce42be6f39783d2bb1438c8d3e48d6c8
User <allah@pidoras>
2024-11-25 14:48:36 +0300

    a

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import cv2
 def prog1():
     from djitellopy import Tello
     import threading, time
+    # Инициализация дрона
     tello = Tello()
     event = threading.Event()
     tello.connect()
@@ -11,32 +12,36 @@ def prog1():
     print(f'Заряд батареи: {tello.get_battery()}%')
     print(f'Температура: {tello.get_temperature()} ℃')
 
+    # Получение кадра
     tello.streamon()
     frame_read = tello.get_frame_read()
     height, width, _ = frame_read.frame.shape
+
+    # Создание объекта для записи видео в файл video_out_1.avi
     video = cv2.VideoWriter('video_out_1.avi', cv2.VideoWriter_fourcc(*'XVID'), 30, (width, height))
 
+    # Осуществление управления движением дрона через клавиатуру
     def keycheck():
         while True:
             event.clear()
             event.wait()
-            if key == 27:
+            if key == 27:  # Выход на Escape
                 break
-            elif key == 13:
+            elif key == 13:  # Посадка дрона на Enter
                 tello.land()
-            elif key == 32:
+            elif key == 32:  # Выключение дрона на Пробел
                 tello.takeoff()
-            elif key == ord('w'):
+            elif key == ord('w'):  # Движение вперёд на W
                 tello.move_forward(50)
-            elif key == ord('s'):
+            elif key == ord('s'):  # Движение назад на S
                 tello.move_back(50)
-            elif key == ord('a'):
+            elif key == ord('a'):  # Поворот налево на A
                 tello.move_left(50)
-            elif key == ord('d'):
+            elif key == ord('d'):  # Поворот направо на D
                 tello.move_right(50)
-            elif key == ord('e'):
+            elif key == ord('e'):  # Поворот по часовой стрелке на 30 градусов клавишей E
                 tello.rotate_clockwise(30)
-            elif key == ord('q'):
+            elif key == ord('q'):  # Поворот против часовой стрелки на 30 градусов клавишей Q
                 tello.rotate_counter_clockwise(30)
             time.sleep(0.6)
 
@@ -51,7 +56,7 @@ def prog1():
             event.set()
             break
         elif key == ord('f'):
-            cv2.imwrite('image_out_1.png', frame)
+            cv2.imwrite('image_out_1.png', frame)  # Сохранение скриншота клавишей F
         elif key == 255:
             pass
         else:
@@ -63,6 +68,7 @@ def prog1():
 
 
 def prog2():
+    # Инициализация модели машинного обучения
     from ultralytics import YOLO
     model = YOLO("roofs.pt")
     results = model('video_in_2.avi', stream=True)
@@ -130,6 +136,7 @@ def prog3():
     video.release()
     tello.streamoff()
     tello.end()
+
 
 # ---- ИНТЕРФЕЙС_ПРОВЕРКИ_ЗАДАНИЙ ----
 while True:
